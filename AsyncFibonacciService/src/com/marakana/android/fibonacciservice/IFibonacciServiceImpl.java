5a09c519f3ada82351126d7d523efa9aaacfe68c
Aleksandar Gargenta <sasa@gargenta.com>
2012-05-07 20:49:49 -0700

    Switched to SystmeClock. Updated JNI code. Added .settings. Updated .gitignore

--- AsyncFibonacciService/src/com/marakana/android/fibonacciservice/IFibonacciServiceImpl.java
+++ AsyncFibonacciService/src/com/marakana/android/fibonacciservice/IFibonacciServiceImpl.java
@@ -1,7 +1,7 @@
-
 package com.marakana.android.fibonacciservice;
 
 import android.os.RemoteException;
+import android.os.SystemClock;
 import android.util.Log;
 
 import com.marakana.android.fibonaccicommon.FibonacciRequest;
@@ -11,33 +11,34 @@ import com.marakana.android.fibonaccicommon.IFibonacciServiceResponseListener;
 import com.marakana.android.fibonaccinative.FibLib;
 
 public class IFibonacciServiceImpl extends IFibonacciService.Stub {
-    private static final String TAG = "IFibonacciServiceImpl";
+	private static final String TAG = "IFibonacciServiceImpl";
 
-    @Override
-    public void fib(FibonacciRequest request, IFibonacciServiceResponseListener listener)
-            throws RemoteException {
-        long n = request.getN();
-        Log.d(TAG, "fib(" + n + ")");
-        long timeInMillis = System.currentTimeMillis();
-        long result;
-        switch (request.getType()) {
-            case FibonacciRequest.ITERATIVE_JAVA_TYPE:
-                result = FibLib.fibJI(n);
-                break;
-            case FibonacciRequest.RECURSIVE_JAVA_TYPE:
-                result = FibLib.fibJR(n);
-                break;
-            case FibonacciRequest.ITERATIVE_NATIVE_TYPE:
-                result = FibLib.fibNI(n);
-                break;
-            case FibonacciRequest.RECURSIVE_NATIVE_TYPE:
-                result = FibLib.fibNR(n);
-                break;
-            default:
-                result = 0;
-        }
-        timeInMillis = System.currentTimeMillis() - timeInMillis;
-        Log.d(TAG, String.format("Got fib(%d) = %d in %d ms", n, result, timeInMillis));
-        listener.onResponse(new FibonacciResponse(result, timeInMillis));
-    }
+	@Override
+	public void fib(FibonacciRequest request,
+			IFibonacciServiceResponseListener listener) throws RemoteException {
+		long n = request.getN();
+		Log.d(TAG, "fib(" + n + ")");
+		long timeInMillis = SystemClock.uptimeMillis();
+		long result;
+		switch (request.getType()) {
+		case ITERATIVE_JAVA:
+			result = FibLib.fibJI(n);
+			break;
+		case RECURSIVE_JAVA:
+			result = FibLib.fibJR(n);
+			break;
+		case ITERATIVE_NATIVE:
+			result = FibLib.fibNI(n);
+			break;
+		case RECURSIVE_NATIVE:
+			result = FibLib.fibNR(n);
+			break;
+		default:
+			result = 0;
+		}
+		timeInMillis = SystemClock.uptimeMillis() - timeInMillis;
+		Log.d(TAG, String.format("Got fib(%d) = %d in %d ms", n, result,
+				timeInMillis));
+		listener.onResponse(new FibonacciResponse(result, timeInMillis));
+	}
 }
